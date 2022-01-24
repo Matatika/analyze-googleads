76dc15d8495a5b700b7b17a72d91efb7e290e776
danielpdwalker <danielpdwalker@gmail.com>
2022-01-24 12:39:49 +0000

    Initial v0.2.0 commit. Reflcted model name changes in datasets. Made datasets support postgres and snowflake. Updated dbt macro name in elt

--- bundle/orchestrate/tap-googleads/elt.sh
+++ bundle/orchestrate/tap-googleads/elt.sh
@@ -23,7 +23,7 @@ export DBT_TARGET_SCHEMA=${TARGET_POSTGRES_SCHEMA}
 meltano invoke dbt deps
 
 # Try to drop the users_stream table to allow dbt snapshot to capture hard deleted users from source
-meltano invoke dbt run-operation drop_google_ads_stream_tables || true
+meltano invoke dbt run-operation googleads_drop_google_ads_stream_tables || true
 
 # Run the elt, and dbt commands and tests
 meltano elt "$EXTRACTOR" "$LOADER" --transform=skip
