aaadb9985c4705611c6495a51845609ecc1f4cee
danielpdwalker <danielpdwalker@gmail.com>
2022-02-03 15:14:51 +0000

    Updated elt.sh to remove setting env vars

--- bundle/orchestrate/tap-googleads/elt.sh
+++ bundle/orchestrate/tap-googleads/elt.sh
@@ -9,16 +9,6 @@ meltano install loader "$LOADER"
 meltano install transform "$EXTRACTOR"
 meltano install transformer dbt
 
-# Transform into our custom target schema (these variables all required by dbt see profiles.yml)
-export DBT_TARGET=postgres
-export PG_ADDRESS=${TARGET_POSTGRES_HOST}
-export PG_PORT=${TARGET_POSTGRES_PORT}
-export PG_USERNAME=${TARGET_POSTGRES_USER}
-export PG_PASSWORD=${TARGET_POSTGRES_PASSWORD}
-export PG_DATABASE=${TARGET_POSTGRES_DBNAME}
-export DBT_SOURCE_SCHEMA=${TARGET_POSTGRES_SCHEMA}
-export DBT_TARGET_SCHEMA=${TARGET_POSTGRES_SCHEMA}
-
 # Install dbt dependencies files
 meltano invoke dbt deps
 
