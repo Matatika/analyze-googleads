5909e0dd3c477eeb5af7488e2cbcc16cb2ef8b20
Daniel Walker <34437496+DanielPDWalker@users.noreply.github.com>
2022-02-23 11:36:47 +0000

    Bumped version to 0.3.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="analyze-googleads",
-    version="0.2.0",
+    version="0.3.0",
     description="Meltano project file bundle of Matatika datasets for Google Ads",
     packages=find_packages(),
     package_data={
