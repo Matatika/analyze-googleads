5e1e622d67383515e0d66a29d7111db0cf506bad
Reuben Frankel <rfrankel@matatika.com>
2022-06-06 16:42:03 +0100

    Remove `MarkupSafe` reinstall temporary fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="analyze-googleads",
-    version="0.4.1",
+    version="0.4.2",
     description="Meltano project file bundle of Matatika datasets for Google Ads",
     packages=find_packages(),
     package_data={
