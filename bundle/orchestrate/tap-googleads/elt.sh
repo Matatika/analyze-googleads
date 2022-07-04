f155e246fc72b255ab332194d78f34e0faaf70c0
ReubenFrankel <60552974+ReubenFrankel@users.noreply.github.com>
2022-07-04 12:09:20 +0100

    Update elt.sh

--- bundle/orchestrate/tap-googleads/elt.sh
+++ bundle/orchestrate/tap-googleads/elt.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 
-# Exit on error
+# exit on error
 set -e
 
-# Meltano setup
-meltano install extractor "$EXTRACTOR"
-meltano install loader "$LOADER"
-meltano install transform "$EXTRACTOR"
-meltano install transformer dbt
-
-# Install dbt dependencies files
+# install dbt dependencies
 meltano invoke dbt deps
 
-# Try to drop the users_stream table to allow dbt snapshot to capture hard deleted users from source
+# try to drop the users_stream table to allow model to capture hard deleted users from source
 meltano invoke dbt run-operation googleads_drop_google_ads_stream_tables || true
 
-# Run the elt, and dbt commands and tests
-meltano elt "$EXTRACTOR" "$LOADER" --transform=skip --job_id="$EXTRACTOR"-"$LOADER"-"$IMPORTRUNNERID"
+# run extract-load
+meltano run tap-googleads "$LOADER"
+
+# run transforms
 meltano invoke dbt run -m tap_googleads
